70bb08e9ca4fa0e603c82808fee5f12727e542dd
James Inaz <jamezslim90@gmail.com>
2024-12-31 09:19:15 +0100

    Updates

--- crueltouch/settings.py
+++ crueltouch/settings.py
@@ -21,7 +21,7 @@ from django.utils.translation import gettext_lazy as _
 
 load_dotenv()
 allowed_hosts_str = os.getenv('LIST_OF_ALLOWED_HOSTS', default="")
-ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1","maxify-appointments.onrender.com"]
+# ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1","maxify-appointments.onrender.com"]
 debug_value = os.getenv('DEBUG_VALUE')
 secret_key_value = os.getenv('SECRET_KEY_VALUE')
 
